078d2368ddd4f8699ab60c20b8ed56d163f5d73d
thekuuri <nganga.n.kuuri@gmail.com>
2025-10-23 14:03:19 +0300

    Remove timestamp from output PDF naming convention in process method

--- app/Http/Controllers/LetterheadController.php
+++ app/Http/Controllers/LetterheadController.php
@@ -21,7 +21,7 @@ class LetterheadController extends Controller
         $uploadedFile = $request->file('pdf');
         $inputPath = $uploadedFile->getRealPath();
         $originalName = pathinfo($uploadedFile->getClientOriginalName(), PATHINFO_FILENAME);
-        $outputPath = storage_path('app/public/outputs/' . $originalName . '_LetterHead_' . time() . '.pdf');
+        $outputPath = storage_path('app/public/outputs/' . $originalName . '.pdf');
         $imagePath = public_path('letterheads/letterhead.png');
 
         $this->addLetterheadImage($inputPath, $imagePath, $outputPath);
